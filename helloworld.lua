d3325a5be36c71fd4ca7babd35dea929a112b232
EvansChang <85141291@qq.com>
2023-03-22 11:56:45 +0800

    add test example

---
+++ helloworld.lua
@@ -0,0 +1,32 @@
+print("Hi")
+local a = 1
+local b = 1
+print(a+b)
+
+local i = 1
+i = i + 1
+print(i)
+
+local phrase = "I am"
+local name = "evans"
+print(phrase.. " " ..name)
+print("I am ".."Sid")
+
+local isAlive = true
+print(isAlive)
+
+local age = 10
+
+if age > 18 then
+  print("over 18")
+end
+
+age = 20
+
+if age >18 then
+  print("over 28")
+elseif age == 18 then
+  print("18 huh")
+else
+print("kiddo")
+end
