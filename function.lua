aa976e2fd47b5c9971f49a3e80e11acf10944846
EvansChang <85141291@qq.com>
2023-03-22 16:12:10 +0800

    add function example

--- function.lua
+++ function.lua
@@ -23,3 +23,46 @@ end
 for j = 1,3 do
   print("hi")
 end
+
+local colors = {"red","green","blue"}
+
+print(colors[1])
+
+for j=1,#colors do
+  print(colors[j])
+end
+
+for index,_ in ipairs(colors) do
+  print(colors[index])
+end
+
+local info = {
+  name ="sid",
+  age=20,
+  isAlive = true
+}
+
+print(info["name"])
+print(info.name)
+
+for key,value in pairs(info) do
+  print(key .. " ".. tostring(value))
+end
+
+local data = {
+  {"sid",20},
+  {"tim",90},
+}
+
+for j=1,#data do
+  print(data[j][1].." is "..data[j][2].." years old")
+end
+
+local data2 = {
+  sid = {age=20},
+  tim={age=90},
+}
+
+for j=1,#data2 do
+  print(data[j]["age"].."is ")
+end
